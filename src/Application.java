d502a478075e9093bced9c918b9adb8464e6093e
DGolf1313 <103085521+DGolf1313@users.noreply.github.com>
2022-09-19 14:27:58 -0500

    2nd commit

--- src/Application.java
+++ src/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		//Integer array 
-		Integer[] arrayTriangleValues = {45,23,3,6,29};
+		Integer[] arrayTriangleValues = {45,23,3,6,29,7,9,22,4};
 		//45,29,23,6,3
 		
 		determineNumberOfTriangles(arrayTriangleValues);
@@ -26,7 +26,7 @@ public class Application {
 		for(int a = 0; a < sides.length; a++)
 		
 		//Print the new order of the array
-		System.out.print(sides[a]);
+		System.out.print(sides[a] + " ");
 		System.out.println();
 		System.out.println();
 		
@@ -41,15 +41,16 @@ public class Application {
 				}
 			}  
 		}
-		
+		System.out.println("The following triangle combination(s) could be made: ");
 		for(int t = 0; t < listOfValidTriangles.size(); t++) {
 			System.out.print(listOfValidTriangles.get(t).getSideA() + ", ");
 			System.out.print(listOfValidTriangles.get(t).getSideB() + ", ");
 			System.out.print(listOfValidTriangles.get(t).getSideC());
 			System.out.println();
-			System.out.println(numTriaglesCreated);
-		}		
-		
+			
+		}
+		System.out.println();
+		System.out.println("The number of Triangles created was: " + numTriaglesCreated);
 		return listOfValidTriangles;
 	}
 }
